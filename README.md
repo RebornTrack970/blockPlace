e390fb3035ea176bd787fc4759463c3667836ebd
RebornTrack970 <62565267+RebornTrack970@users.noreply.github.com>
2021-02-02 11:10:37 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,12 @@
+#Thanks for UnitSpawn plugin for Template.
+
+# Unit Spawn
+![Build](https://github.com/RebornTrack970/blockPlace/workflows/Build/badge.svg)
+## Use
+
+Template: `/place block team x y`
+
+Ex: `/place router sharded 10 10`
+
+## Installation
+./gradlew jar 
---
+++ src/main/java/blockPlace.java
@@ -0,0 +1,65 @@
+
+import arc.util.*;
+import mindustry.Vars;
+import mindustry.game.Team;
+import mindustry.gen.*;
+import mindustry.mod.Mod;
+import mindustry.world.*;
+
+public class blockPlace extends Mod {
+
+    @Override
+    public void registerClientCommands(CommandHandler handler){
+        handler.<Player>register( "place", "<block> <team> <x> <y>", "Place blocks", (args, player) ->{
+
+            Block sblock = Vars.content.blocks().
+                    find(b -> b.name.equals(args[0]));
+	    int xx;
+	    int yy;
+	    try {
+                xx = Integer.parseInt(args[2]);
+            } catch (NumberFormatException e) {
+                player.sendMessage("[red]X must be number!");
+                return;
+            }
+	    try {
+                yy = Integer.parseInt(args[3]);
+            } catch (NumberFormatException e) {
+                player.sendMessage("[red]Y must be number!");
+                return;
+            }
+            Team tteam;
+            switch (args[2]) {
+                case "sharded":
+                    tteam = Team.sharded;
+                    break;
+                case "blue":
+                    tteam = Team.blue;
+                    break;
+                case "crux":
+                    tteam = Team.crux;
+                    break;
+                case "derelict":
+                    tteam = Team.derelict;
+                    break;
+                case "green":
+                    tteam = Team.green;
+                    break;
+                case "purple":
+                    tteam = Team.purple;
+                    break;
+                default:
+                    player.sendMessage("[accent]Teams: [yellow]sharded[], [blue]blue[], [red]crux[], [gray]derelict[], [green]green[], [purple]purple[].");
+                    return;
+            }
+
+            if (sblock != null) {
+                Vars.world.tile(xx,yy).setNet(sblock,tteam,0);
+                player.sendMessage("[royal]You are placing" + " " +"[accent]"+sblock + " " + "[green]for" + " " +"[accent]"+tteam + " " + "[royal]team at "+xx+","+yy);
+            } else {
+                player.sendMessage("[red]You may select a Block.");
+            }
+
+        });
+    }
+}
